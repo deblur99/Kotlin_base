d51f2843a29c8694dfb34a86d11e1500074fb956
deblur99 <deblur99@kakao.com>
2021-11-01 22:56:14 +0900

    added-11-2

--- app/src/main/java/com/example/practiceapp/Intent1.kt
+++ app/src/main/java/com/example/practiceapp/Intent1.kt
@@ -1,9 +1,12 @@
 package com.example.practiceapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_intent.*
+import kotlinx.android.synthetic.main.activity_intent2.*
 
 class Intent1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,21 +14,38 @@ class Intent1 : AppCompatActivity() {
         setContentView(R.layout.activity_intent)
 
         change_activity.setOnClickListener {
-            val intent = Intent(this@Intent1, Intent2::class.java)
+//            val intent = Intent(this@Intent1, Intent2::class.java)
+//
+//            // putExtra() 함수로 Key, Value 방식으로 목적지 Intent 에 값을 전달할 수 있다.
+////            intent.putExtra("number1", 1)
+////            intent.putExtra("number2", 2)
+////            startActivity(intent)
+//
+//            val intent2 = Intent(this@Intent1, Intent2::class.java)
+//            // Intent 객체의 apply 메서드는 여러 개의 putExtra 를 하나로 묶을 수 있다.
+//            // 즉, intent2 에 관련된 모든 정보들을 여기에 담음으로써, 가독성이 올라간다.
+//            intent2.apply {
+//                this.putExtra("number1", 1)
+//                this.putExtra("number2", 2)
+//            }
+//            startActivityForResult(intent2, 200)
 
-            // putExtra() 함수로 Key, Value 방식으로 목적지 Intent 에 값을 전달할 수 있다.
-            intent.putExtra("number1", 1)
-            intent.putExtra("number2", 2)
+            // 암시적 인텐트
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
             startActivity(intent)
-
-            val intent2 = Intent(this@Intent1, Intent2::class.java)
-            // Intent 객체의 apply 메서드는 여러 개의 putExtra 를 하나로 묶을 수 있다.
-            // 즉, intent2 에 관련된 모든 정보들을 여기에 담음으로써, 가독성이 올라간다.
-            intent2.apply {
-                this.putExtra("number1", 1)
-                this.putExtra("number2", 2)
-            }
-            startActivity(intent2)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 200) {
+            Log.d("requestCode number", requestCode.toString())
+            Log.d("resultCode number", resultCode.toString())
+
+            val result = data?.getIntExtra("result", 0)
+            Log.d("number", result.toString())
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
