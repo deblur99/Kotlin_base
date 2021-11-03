3463943790b55bfa3df65b065675637d0fb0a7b4
deblur99 <deblur99@kakao.com>
2021-11-03 18:34:10 +0900

    added-11-3

---
+++ app/src/main/java/com/example/practiceapp/Actual/TeamCreation.kt
@@ -0,0 +1,12 @@
+package com.example.practiceapp.Actual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practiceapp.R
+
+class TeamCreation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_creation)
+    }
+}
